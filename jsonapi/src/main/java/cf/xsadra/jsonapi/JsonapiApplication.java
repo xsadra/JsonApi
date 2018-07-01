670435216e59aa56e3f3fa2950b722b066e34deb
Sadra <ti9mail@gmail.com>
2018-07-01 20:27:47 +0200

    init template

---
+++ jsonapi/src/main/java/cf/xsadra/jsonapi/JsonapiApplication.java
@@ -0,0 +1,12 @@
+package cf.xsadra.jsonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonapiApplication.class, args);
+	}
+}
